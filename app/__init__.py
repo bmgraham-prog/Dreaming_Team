c85aa4e49c95df9fddddd1f38f3bc5e8879aef2e
bmgraham-prog <isaacbarare@gmail.com>
2020-07-29 06:23:35 -0700

    added the gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 # app/__init__.py
 
 # third-party imports
-import os
-from flask import Flask,render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -16,16 +15,9 @@ login_manager = LoginManager()
 
 
 def create_app(config_name):
-    if os.getenv('FLASK_CONFIG') == "production":
-        app = Flask(__name__)
-        app.config.update(
-            SECRET_KEY=os.getenv('SECRET_KEY'),
-            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
-        )
-    else:
-        app = Flask(__name__, instance_relative_config=True)
-        app.config.from_object(app_config[config_name])
-        app.config.from_pyfile('config.py')
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
 
     Bootstrap(app)
     db.init_app(app)
@@ -44,18 +36,5 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
-
-
-    @app.errorhandler(403)
-    def forbidden(error):
-        return render_template('errors/403.html', title='Forbidden'), 403
-
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return render_template('errors/404.html', title='Page Not Found'), 404
-
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return render_template('errors/500.html', title='Server Error'), 500
 
     return app
